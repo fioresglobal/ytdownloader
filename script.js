5e4c8a96f6815ea1382e3ee43a36e7eabdb6f056
bilgehanors <130554082+bilgehanors@users.noreply.github.com>
2023-04-24 01:42:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var convertBtn = document.querySelector('.convert-button');
+var URLinput = document.querySelector('.txt');
+convertBtn.addEventListener('click', () => {
+    console.log(`URL: ${URLinput.value}`);
+    sendURL(URLinput.value);
+});
+function sendURL(URL) {
+    window.location.href = `http://localhost:4000/download?URL=${URL}`;
+}
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
+
+
