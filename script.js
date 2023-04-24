9febd2ef7a104bbdd7dd182ab7cca754766e09ca
bilgehanors <130554082+bilgehanors@users.noreply.github.com>
2023-04-24 13:31:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-var convertBtn = document.querySelector('.convert-button');
-var URLinput = document.querySelector('.txt');
-convertBtn.addEventListener('click', () => {
-    console.log(`URL: ${URLinput.value}`);
-    sendURL(URLinput.value);
-});
-function sendURL(URL) {
-    window.location.href = `http://localhost:4000/download?URL=${URL}`;
-}
+
 window.onscroll = function() {myFunction()};
 
 var header = document.getElementById("myHeader");
